63d330e58cc6a48b027bc4ec24fb27e7237629f6
Zineb BALADOU <zinebbaladou@gmail.com>
2020-11-09 16:24:43 +0100

    Menu componenet V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ //component decorator
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',  //template:`<h1>{{title}}</h1>...`
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
