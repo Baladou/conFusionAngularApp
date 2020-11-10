649af98c2ae69510d3ca7ce32a6e9984fe6b8ae2
Zineb BALADOU <zinebbaladou@gmail.com>
2020-11-10 10:25:54 +0100

    Angular Router Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -16,6 +16,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +27,9 @@ import { FooterComponent } from './footer/footer.component';
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -34,6 +40,7 @@ import { FooterComponent } from './footer/footer.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    AppRoutingModule
   ],
   //Services here in the provider
   //make a service accessible for all the components
