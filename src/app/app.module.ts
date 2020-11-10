d08cc84110a7233895f337061368d9572f2e085f
Zineb BALADOU <zinebbaladou@gmail.com>
2020-11-10 21:41:45 +0100

    Promise Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
@@ -60,6 +61,7 @@ import { LoginComponent } from './login/login.component';
     MatDialogModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatProgressSpinnerModule,
 
     AppRoutingModule,
     FormsModule ,
