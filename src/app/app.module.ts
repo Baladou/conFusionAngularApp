1a5e56399a6f200996a0b8fc1fada0a925f028d3
Zineb BALADOU <zinebbaladou@gmail.com>
2020-11-10 11:11:41 +0100

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,14 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -44,7 +46,7 @@ import { HomeComponent } from './home/home.component';
   ],
   //Services here in the provider
   //make a service accessible for all the components
-  providers: [DishService],
+  providers: [DishService,PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
