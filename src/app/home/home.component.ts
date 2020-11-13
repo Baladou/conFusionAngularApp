9bdeced90b30cb2c91b42fc8aec2d1fe763fd8a2
Zineb BALADOU <zinebbaladou@gmail.com>
2020-11-13 16:30:48 +0100

    Animations Part 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,10 +7,17 @@ import { PromotionService } from '../services/promotion.service';
 import { Leader } from '../shared/leader';
 import { LeaderService } from '../services/leader.service';
 import { d } from '@angular/core/src/render3';
+
+import { flyInOut,expand } from '../animations/app.animation';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class HomeComponent implements OnInit {
 
